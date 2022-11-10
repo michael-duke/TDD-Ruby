4a63c81d43c496eefb04e09484391b5ff88141aa
samiullah997 <samiullahk997@gmail.com>
2022-11-10 18:54:56 +0500

    Add logic in the #fizzbuzz method for number divisible by 3

--- solver.rb
+++ solver.rb
@@ -6,4 +6,10 @@ class Solver
   def reverse(word)
     word.instance_of?(String) ? word.reverse : (raise StandardError, 'Word Not A String Error')
   end
+
+  def fizzbuzz(number)
+    if (number%3).zero?
+      'fizz'
+    end
+  end
 end
