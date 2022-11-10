be6d1492d79e219dea72843501cfac29c8d7cc69
samiullah997 <samiullahk997@gmail.com>
2022-11-10 18:13:31 +0500

    Implement #reverse method after following TDD approach

--- solver.rb
+++ solver.rb
@@ -2,4 +2,8 @@ class Solver
   def factorial(number)
     number.negative? ? (raise StandardError, 'NegativeNumberFactorialError') : (1..number).reduce(1, :*)
   end
+
+  def reverse(word)
+    word.reverse
+  end
 end
